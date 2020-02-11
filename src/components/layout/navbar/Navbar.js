89313e33156a07a70cbe6cdb65c5746e72115966
Chris Armendarez <chris.michael.armo@gmail.com>
2020-02-11 00:21:50 -0800

    Add navbar prop types

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Navbar extends Component {
   static defaultProps = {
     title: 'Application Name',
     icon: 'fas fa-laptop-code',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
   };
 
   render() {
