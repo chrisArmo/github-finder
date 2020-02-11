5ed61bd2a6450fbc2bf865d8032fb567ee9b3b96
Chris Armendarez <chris.michael.armo@gmail.com>
2020-02-11 00:23:06 -0800

    Add container with user list implementation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from './components/layout/navbar/Navbar';
+import UserList from './components/users/user-list/UserList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
+
+        <div className="container">
+          <UserList />
+        </div>
       </div>
     );
   }
