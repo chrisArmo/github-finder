bb946d69589153c83ffef4d253b92910e6cd668c
Chris Armendarez <chris.michael.armo@gmail.com>
2020-02-12 14:30:56 -0800

    Add prop types & refactor

--- src/components/users/user-item/UserItem.js
+++ src/components/users/user-item/UserItem.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const UserItem = ({ id, login, avatar_url, html_url }) => {
-  return (
-    <div className="card text-center">
-      <img
-        className="round-img"
-        src={avatar_url}
-        alt="profile avatar"
-        style={{ width: '150px' }}
-      />
+const UserItem = ({ login, avatar_url, html_url }) => (
+  <div className="card text-center">
+    <img
+      className="round-img"
+      src={avatar_url}
+      alt="profile avatar"
+      style={{ width: '125px' }}
+    />
 
-      <h4>{login}</h4>
+    <h4>{login}</h4>
 
-      <div>
-        <a
-          className="btn btn-dark btn-sm my-1"
-          href={html_url}
-          target="_blank"
-        >
-          See profile.
-        </a>
-      </div>
+    <div>
+      <a className="btn btn-dark btn-sm my-1" href={html_url}>
+        See profile.
+      </a>
     </div>
-  );
+  </div>
+);
+
+UserItem.propTypes = {
+  login: PropTypes.string.isRequired,
+  avatar_url: PropTypes.string.isRequired,
+  html_url: PropTypes.string.isRequired,
 };
 
 export default UserItem;
