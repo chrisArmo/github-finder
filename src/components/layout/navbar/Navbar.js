00b1df421f09cb9daf0966dca7285f35c407bc8b
Chris Armendarez <chris.michael.armo@gmail.com>
2020-02-12 13:50:55 -0800

    Refactor to functional component

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-  static defaultProps = {
-    title: 'Application Name',
-    icon: 'fas fa-laptop-code',
-  };
+const Navbar = ({ title, icon }) => (
+  <nav className="navbar bg-primary">
+    <h1>
+      <i className={icon}></i> {title}
+    </h1>
+  </nav>
+);
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+Navbar.defaultProps = {
+  title: 'Application Name',
+  icon: 'fas fa-laptop-code',
+};
 
-  render() {
-    const { title, icon } = this.props;
-
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={icon}></i> {title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
