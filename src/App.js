877db82057f36a309c31de78b9d43b1ba6be11d4
Chris Armendarez <chris.michael.armo@gmail.com>
2020-02-23 13:01:03 -0800

    Fetch github users with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
@@ -6,13 +7,34 @@ import Navbar from './components/layout/navbar/Navbar';
 import UserList from './components/users/user-list/UserList';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState(() => ({ loading: true }));
+
+      const { data: users = [] } = await axios.get(
+        'https://api.github.com/users',
+      );
+
+      this.setState(() => ({ users, loading: false }));
+    } catch ({ message }) {
+      console.log('error:', message);
+    }
+  }
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
 
         <div className="container">
-          <UserList />
+          <UserList users={users} loading={loading} />
         </div>
       </div>
     );
