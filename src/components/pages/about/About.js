19a00570b1d61e41ead5b3997d07e6c84b76faf4
Chris Armendarez <chris.michael.armo@gmail.com>
2020-03-07 23:23:06 -0800

    Add about page component

---
+++ src/components/pages/about/About.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+        explicabo nesciunt reiciendis voluptas cupiditate sunt, placeat
+        perspiciatis harum voluptatum? Necessitatibus voluptates vitae magni
+        iste quia unde eius illum laboriosam eveniet.
+      </p>
+
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
